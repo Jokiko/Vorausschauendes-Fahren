1844dc9c29087642b569aae3550be608e8ce0e6f
Jokiko <75935156+Jokiko@users.noreply.github.com>
2023-08-08 12:29:15 +0200

    Camera Movement
    -added camera movement that follows the car from inside

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -14,11 +14,17 @@ public class CameraMovement : MonoBehaviour
     private float rotationX = 0f;
     private float rotationY = 0f;
 
+    private GameObject car; 
+    private Vector3 cameraOffset = new Vector3(0.1f, 1.35f, 0.0f);
+
     void Start()
     {
         // Lock and hide the cursor to prevent it from leaving the game window
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+
+        car = GameObject.Find("MSH_Blue_Sedan");
+        //cameraOffset = new Vector3(0.0f, 1.0f, 0.0f);
     }
 
     void Update()
@@ -28,13 +34,17 @@ public class CameraMovement : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * sensitivityY;
 
         // Calculate the new rotation values based on mouse movement
-        rotationX += mouseX;
-        rotationY -= mouseY;
+        //rotationX += mouseX;
+        //rotationY -= mouseY;
 
         // Clamp the vertical rotation to avoid flipping the camera
-        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        //rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
 
         // Apply the rotation to the camera
-        transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0f);
+        //transform.localRotation = Quaternion.Euler(rotationY, rotationX, 0f);
+
+        //transform.position = car.transform.position + cameraOffset;
+        transform.position = Vector3.Lerp(transform.position, car.transform.position + cameraOffset, 1.0f);
+        transform.rotation = car.transform.rotation;
     }
 }
