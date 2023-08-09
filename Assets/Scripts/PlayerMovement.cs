938c9a6b3540e8fe8449c5ab7a35a2616e96f3d3
Jokiko <75935156+Jokiko@users.noreply.github.com>
2023-08-09 16:56:01 +0200

    Win & Lose Sceens
    -added win and lose screens
    -changed braking to single press

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,33 +10,58 @@ public class PlayerMovement : MonoBehaviour
     public float spacebarBrakeForce = 10f; // Brake force when using spacebar to slow down
     public float turnSpeed = 50f; // Turning speed
 
+    private float accelerationInput;
+    private float turnInput;
+    private bool spacebarPressed;
+
     private Rigidbody rb;
     private float currentSpeed = 0f;
+
+    private GameObject loseScreen;
+    private GameObject winScreen;
+    private GameObject tooEarlyScreen;
+
+    private float distance = 0.0f;
+    public TextMeshProUGUI winText;
+
+    public bool playerControEnabled = true;
 
 
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        loseScreen = GameObject.Find("LoseCanvas");
+        loseScreen.SetActive(false);
+        winScreen = GameObject.Find("WinCanvas");
+        winScreen.SetActive(false);
+        winText.text = $"Das waren noch {distance}m bis zum Crash";
+        tooEarlyScreen = GameObject.Find("TooEarlyCanvas");
+        tooEarlyScreen.SetActive(false);
     }
 
     private void FixedUpdate()
     {
-        // Input handling
-        float accelerationInput = Input.GetAxis("Vertical");
-        float turnInput = Input.GetAxis("Horizontal");
-        bool spacebarPressed = Input.GetKey(KeyCode.Space);
+        if(playerControEnabled && !spacebarPressed){
+            // Input handling
+            accelerationInput = Input.GetAxis("Vertical");
+            turnInput = Input.GetAxis("Horizontal");
+            if(Input.GetKey(KeyCode.Space)){
+                spacebarPressed = true;
+            }
+
+        }
 
         // Check for acceleration input (W or S keys)
-        if (accelerationInput > 0f)
-        {
-            // Accelerate
-            currentSpeed = Mathf.Clamp(currentSpeed + acceleration * Time.deltaTime, 0f, maxSpeed);
-        }
-        else if (accelerationInput < 0f || spacebarPressed) // Brake when pressing S or Spacebar
+        if (accelerationInput < 0f || spacebarPressed) // Brake when pressing S or Spacebar
         {
             // Brake
             currentSpeed = Mathf.Clamp(currentSpeed - spacebarBrakeForce * Time.deltaTime, 0f, maxSpeed);
+        }
+        else if (accelerationInput > 0f)
+        {
+            // Accelerate
+            currentSpeed = Mathf.Clamp(currentSpeed + acceleration * Time.deltaTime, 0f, maxSpeed);
         }
         
 
@@ -50,11 +76,21 @@ public class PlayerMovement : MonoBehaviour
             Quaternion turnRotation = Quaternion.Euler(0f, turnAngle, 0f);
             rb.MoveRotation(rb.rotation * turnRotation);
         }
+
+        //check for win condition
+        if (currentSpeed <= 0f && spacebarPressed){
+            tooEarlyScreen.SetActive(true);
+        }
     }
 
     private void OnCollisionEnter(Collision collision){
         
         Debug.Log("Kollision!");
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {   
+            loseScreen.SetActive(true);
+            playerControEnabled = false;
+        } 
         
     }
 
